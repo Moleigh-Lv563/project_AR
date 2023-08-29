9679125f00a746d3c325dc0a967909a0da4a2d65
Moleigh-Lv563 <moleigh.lv563@gmail.com>
2023-08-29 10:58:19 +0900

    update

--- _include/include.js
+++ _include/include.js
@@ -1,13 +1,4 @@
-/*(function(d) {
-  var config = {
-    kitId: 'gah6iet',
-    scriptTimeout: 3000,
-    async: true
-  },
-    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-})(document);
-*/
-
+/* ハンバーガーメニュー */
 $('.sp-button').click(function() {
   $('.bt-bar1').toggleClass('bt-bar1-on'),
   $('.bt-bar2').toggleClass('bt-bar2-on'),
@@ -15,40 +6,32 @@ $('.sp-button').click(function() {
   $('.nav-sp').toggleClass('open');
 });
 
+/* 画面の横サイズによってヘッダーの表示を変更 */
 $(window).on('load', function(){
   $('.container').addClass('loaded').delay(0).queue(function(){
     $('.header').addClass('h-load').dequeue();
   });
 });
 
-$('.nav a[href*="#"], .bt-overview a[href*="#"]').click(function () {
+/* ページ内リンクを踏んだ時、滑らかに移動 */
+$('.nav a[href*="#"], .top-bt a[href*="#"]').click(function () {
 	var elmHash = $(this).attr('href');
 	var pos = $(elmHash).offset().top;
 	$('body,html').animate({scrollTop: pos}, 500);
 	return false;
 });
 
-var WinH = window.innerHeight,
-    WinW = window.innerWidth;
+(jQuery);
 
-(function($) {
-  $.fn.acs = function(options) {
-    var elements = this;
-    var defaults = {
-      screenPos: 0.9,
-      className: 'visible-y0op1'
-    };
-    var setting = $.extend(defaults, options);
-    $(window).on('load scroll', function() {
-      add_class_in_scrolling();
-    });
-    function add_class_in_scrolling() {
-      var winScroll = $(window).scrollTop();
-      var winHeight = $(window).height();
-      var scrollPos = winScroll + (winHeight * setting.screenPos);
-      if(elements.offset().top < scrollPos) {
-        elements.addClass(setting.className);
+/* マーカーラインのアニメーション */
+window.addEventListener('scroll',function() {
+  const height = window.innerHeight;
+  const scroll = this.scrollY;
+  const marker = document.querySelectorAll('.marker');
+  const value = scroll - height + 300;
+  marker.forEach(function(element){
+      if (scroll > element.getBoundingClientRect().top + value) {
+          element.classList.add('on')
       }
-    }
-  }
-})(jQuery);
+  });
+})
--- app/ar-app.js
+++ app/ar-app.js
@@ -1,34 +1,110 @@
-/*
-  This is your site JavaScript code - you can add interactivity!
-*/
+/****** titleScript ******/
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
 console.log("Hello 🌎");
 
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the steps in the TODO 🚧
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-if (btn) { // Detect clicks on the button
+const btn = document.querySelector("button");
+if (btn) {
   btn.onclick = function () {
-    // The 'dipped' class in style.css changes the appearance on click
     btn.classList.toggle("dipped");
   };
 }
 
-
-// ----- GLITCH STARTER PROJECT HELPER CODE -----
-
-// Open file when the link in the preview is clicked
 let goto = (file, line) => {
   window.parent.postMessage(
     { type: "glitch/go-to-line", payload: { filePath: file, line: line } }, "*"
   );
 };
-// Get the file opening button from its class name
+
 const filer = document.querySelectorAll(".fileopener");
 filer.forEach((f) => {
   f.onclick = () => { goto(f.dataset.file, f.dataset.line); };
+});
+
+/***** mainScript *****/
+
+const showInfo = () => {}
+const showPortfolio = (done) => {
+  const portfolio = document.querySelector("#portfolio-panel");
+  const portfolio1 = document.querySelector("#portfolio-panel1");
+  const btn_left = document.querySelector(".leftButton");
+  const btn_right = document.querySelector(".rightButton");
+  const paintandquestPreviewButton = document.querySelector("#paintandquest-preview-button");
+  const krapi0 = document.querySelector("#portfolio-item0")
+  const krapi1 = document.querySelector("#portfolio-item1")
+  const krapi2 = document.querySelector("#portfolio-item2")
+
+  let y = 0;
+  let currentItem = 0;
+
+  portfolio.setAttribute("visible", true);
+  portfolio1.setAttribute("visible", true);
+
+  const showPortfolioItem = (item) => {
+    for (let i = 0; i <= 2; i++) {
+      document.querySelector("#portfolio-item" + i).setAttribute("visible", i === item);
+    }
+  }
+  const id = setInterval(() => {
+    y += 0;
+    if (y >= 0) {
+      clearInterval(id);
+      btn_left.setAttribute("visible", true);
+      btn_right.setAttribute("visible", true);
+      btn_left.addEventListener('click', () => {
+        currentItem = (currentItem + 1) % 3;
+        showPortfolioItem(currentItem);
+      });
+      btn_right.addEventListener('click', () => {
+        currentItem = (currentItem - 1 + 3) % 3;
+        showPortfolioItem(currentItem);
+      });
+      krapi0.addEventListener("click", function (evt) {
+      window.location.href="https://softmind.tech";
+      })
+      krapi1.addEventListener("click", function (evt) {
+      window.location.href="https://softmind.tech";
+      })
+      krapi2.addEventListener("click", function (evt) {
+      window.location.href="https://softmind.tech";
+      })
+      
+      setTimeout(() => {
+        done();
+      }, 0);
+    }
+    portfolio.setAttribute("position", "0 " + y + " -0.01");
+  }, 10);
+}
+
+const showAvatar = (onDone) => {
+  const avatar = document.querySelector("#avatar");
+  let z = -0.3;
+  const id = setInterval(() => {
+    z += 0.008;
+    if (z >= 0.3) {
+      clearInterval(id);
+      onDone();
+    }
+    avatar.setAttribute("position", "0 -0.25 " + z);
+  }, 10);
+}
+
+AFRAME.registerComponent('mytarget', {
+  init: function () {
+    this.el.addEventListener('targetFound', event => {
+      console.log("target found");
+      showAvatar(() => {
+        setTimeout(() => {
+          showPortfolio(() => {
+            setTimeout(() => {
+              showInfo();
+            }, 300);
+          });
+        }, 300);
+      });
+    });
+    this.el.addEventListener('targetLost', event => {
+      console.log("target found");
+    });
+  }
 });
